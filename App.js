fb8aa6f449d09b612d84d64f797b543824356a75
Tracy Herbert <tracynherbert@gmail.com>
2018-09-12 10:55:51 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Tracy is learning to program apps.</Text>
+        <Text>This is my first app.</Text>
+        <Text>Hello Levi and Skittles.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
